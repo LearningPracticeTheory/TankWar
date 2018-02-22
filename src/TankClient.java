6024f27acc8e03c695df9444b22419debcc627fd
TinyA <1097912818@qq.com>
2018-02-22 16:02:43 +0800

    TC code structure optimization && flash processing

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,10 +1,10 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.swing.JFrame;
 
 public class TankClient extends JFrame {
@@ -12,25 +12,16 @@ public class TankClient extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public final int GAME_WIDTH = 800;
 	public final int GAME_HEIGHT = 600;
-//	private final int TANK_FIRST_NUM = 5;
-	
-//	int x = 50, y = 50;
-	
-	Image img = null;
-	
-	Wall w1 = new Wall(300, 100, 300, 30, this);
-	Wall w2 = new Wall(200, 250, 30, 200, this);
-	List<Wall> walls = new ArrayList<Wall>();
 	
 	Blood b = new Blood(this);
-	
 	Tank myTank = new Tank(50, 50, true, this);
-//	Tank enemyTank = new Tank(100, 100, false, this);
+
+	Wall w1 = new Wall(300, 100, 300, 30, this);
+	Wall w2 = new Wall(200, 250, 30, 200, this);
+
+	List<Wall> walls = new ArrayList<Wall>();
 	List<Tank> tanks = new ArrayList<Tank>();
-	
-//	Missile m = null;
 	List<Missile> missiles = new ArrayList<Missile>();
-//	Explode explode = new Explode(100, 100, this);
 	List<Explode> explodes = new ArrayList<Explode>();
 	
 	public static void main(String args[]) {
@@ -38,18 +29,6 @@ public class TankClient extends JFrame {
 	}
 	
 	TankClient() {
-		/*
-		Properties props = new Properties();
-		try {
-			props.load(this.getClass().getClassLoader().getResourceAsStream("config/tank.properties"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		int initTankNum = Integer.parseInt(props.getProperty("initTankNum"));
-		*/
-		int initTankNum = Integer.parseInt(PropertiesManage.getProperty("initTankNum"));
-//		setSize(800, 600);
 		setSize(GAME_WIDTH, GAME_HEIGHT);
 		setTitle("TankWar");
 		setResizable(false);
@@ -58,6 +37,7 @@ public class TankClient extends JFrame {
 		getContentPane().setBackground(Color.BLACK);
 		setVisible(true);
 		
+		int initTankNum = Integer.parseInt(PropertiesManage.getProperty("initTankNum"));
 		for(int i = 0; i < initTankNum; i++) {
 			tanks.add(new Tank(50 + (i + 1) * 40, 50, false, this));
 		}
@@ -72,18 +52,14 @@ public class TankClient extends JFrame {
 	public void paint(Graphics g) {
 		super.paint(g);
 
-		Color c = g.getColor();
-		g.setColor(Color.WHITE);
-		g.drawString("Missiles count:" + missiles.size(), 10, 50);
-		g.drawString("Tanks count:" + tanks.size(), 10, 70);
-		g.drawString("myTank's life:" + myTank.getLife(), 10, 90);
-		g.setColor(c);
-
 		myTank.draw(g);
 		w1.draw(g);
 		w2.draw(g);
 		b.draw(g);
-//		enemyTank.draw(g);
+
+		myTank.collideWithTanks(tanks);
+		myTank.eatBlood(b);
+
 		for(int i = 0; i < tanks.size(); i++) {
 			Tank t = tanks.get(i);
 			t.draw(g);
@@ -92,73 +68,48 @@ public class TankClient extends JFrame {
 			t.collideWithTanks(tanks);
 		}
 		
-		myTank.collideWithTanks(tanks);
-		myTank.eatBlood(b);
-		
-//		if(m != null) m.draw(g);
 		for(int i = 0; i < missiles.size(); i++) {
 			Missile m = missiles.get(i);
 			m.draw(g);
-			/*
-			if(m.hitTank(enemyTank) && enemyTank.isLive()) {
-				m.setLive(false);
-				enemyTank.setLive(false);
-			}
-			*/
 			m.hitTanks(tanks);
 			m.hitTank(myTank);
 			m.hitWall(w1);
 			m.hitWall(w2);
 		}
 		
-//		explode.draw(g);
 		for(int i = 0; i < explodes.size(); i++) {
 			explodes.get(i).draw(g);
 		}
 		
-//		y += 8;
-	}
-	/*
-	@Override
-	public void update(Graphics g) {
-		if(img == null) {
-			img = this.createImage(WIDTH, HEIGHT);//JFrame
-		}
-		Graphics offScreenGraphics = img.getGraphics();
-		Color c = offScreenGraphics.getColor();
-		offScreenGraphics.setColor(Color.BLACK);
-		offScreenGraphics.fillRect(0, 0, WIDTH, HEIGHT);
-		paint(offScreenGraphics);
-		offScreenGraphics.setColor(c);
-		g.drawImage(img, 0, 0, null);
-	}
-	*/
-	private class PaintThread implements Runnable {
+		Color c = g.getColor();
+		g.setColor(Color.WHITE);
+		g.drawString("Missiles count:" + missiles.size(), 10, 50);
+		g.drawString("Tanks count:" + tanks.size(), 10, 70);
+		g.drawString("myTank's life:" + myTank.getLife(), 10, 90);
+		g.setColor(c);
 
-		@Override
+	}
+
+	private class PaintThread implements Runnable {
 		public void run() {
 			while(true) {
 				repaint();
 				try {
 					Thread.sleep(50);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
-		
 	}
+
 //key pressed && released on Tank will make more stable 
-	
 	private class KeyMonitor extends KeyAdapter {
 
-		@Override
 		public void keyPressed(KeyEvent e) {
 			myTank.keyPressed(e);
 		}
 
-		@Override
 		public void keyReleased(KeyEvent e) {
 			myTank.keyReleased(e);
 		}
