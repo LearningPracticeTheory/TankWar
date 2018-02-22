1ce37ee1786c89283eb8dc1ec54d674b5f240e16
TinyA <1097912818@qq.com>
2018-02-22 15:14:02 +0800

    overwrite update && new RepaintThread

--- src/TankClient.java
+++ src/TankClient.java
@@ -7,6 +7,10 @@ public class TankClient extends JFrame {
 	public final int GAME_WIDTH = 800;
 	public final int GAME_HEIGHT = 600;
 	
+	int x = 50, y = 50;
+	
+	Image img = null;
+	
 	public static void main(String args[]) {
 		new TankClient();
 	}
@@ -20,14 +24,48 @@ public class TankClient extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		getContentPane().setBackground(Color.BLACK);
 		setVisible(true);
+		new Thread(new PaintThread()).start();
 	}
 
 	public void paint(Graphics g) {
 		super.paint(g);
 		Color c = g.getColor();
 		g.setColor(Color.RED);
-		g.fillOval(50, 50, 30, 30);
+		g.fillOval(x, y, 30, 30);
 		g.setColor(c);
+		
+		y += 8;
 	}
-	
+	/*
+	@Override
+	public void update(Graphics g) {
+		if(img == null) {
+			img = this.createImage(WIDTH, HEIGHT);//JFrame
+		}
+		Graphics offScreenGraphics = img.getGraphics();
+		Color c = offScreenGraphics.getColor();
+		offScreenGraphics.setColor(Color.BLACK);
+		offScreenGraphics.fillRect(0, 0, WIDTH, HEIGHT);
+		paint(offScreenGraphics);
+		offScreenGraphics.setColor(c);
+		g.drawImage(img, 0, 0, null);
+	}
+	*/
+	private class PaintThread implements Runnable {
+
+		@Override
+		public void run() {
+			while(true) {
+				repaint();
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
 }
