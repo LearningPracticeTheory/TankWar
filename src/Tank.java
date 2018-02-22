2ee1b690784f9bd03f68bfad502165e3ee361726
TinyA <1097912818@qq.com>
2018-02-22 15:18:18 +0800

    T 8 move directions

--- src/Tank.java
+++ src/Tank.java
@@ -3,8 +3,15 @@ import java.awt.event.*;
 
 public class Tank {
 	
+	public static final int SPEED = 10;
+	
 	private int x, y;
 	TankClient tc = null;
+	
+	boolean bU = false, bD = false, bL = false, bR = false;
+	
+	enum Direction {U, UL, UR, D, DL, DR, L, R, STOP};
+	Direction dir = Direction.STOP;
 	
 	public Tank(int x, int y, TankClient tc) {
 		this.x = x;
@@ -17,6 +24,8 @@ public class Tank {
 		g.setColor(Color.RED);
 		g.fillOval(x, y, 30, 30);
 		g.setColor(c);
+		
+		move();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -24,16 +33,65 @@ public class Tank {
 		int key = e.getKeyCode();
 		switch(key) {
 		case KeyEvent.VK_UP :
-			y -= 8;
+			bU = true;
 			break;
 		case KeyEvent.VK_DOWN :
-			y += 8;
+			bD = true;
 			break;
 		case KeyEvent.VK_LEFT :
-			x -= 8;
+			bL = true;
 			break;
 		case KeyEvent.VK_RIGHT :
-			x += 8;
+			bR = true;
+			break;
+		}
+		direction();
+		
+	}
+
+	public void direction() {
+		if(bU && !bD && !bL && !bR) dir = Direction.U;
+		else if(bU && !bD && bL && !bR) dir = Direction.UL;
+		else if(bU && !bD && !bL && bR) dir = Direction.UR;
+		else if(!bU && bD && !bL && !bR) dir = Direction.D;
+		else if(!bU && bD && bL && !bR) dir = Direction.DL;
+		else if(!bU && bD && !bL && bR) dir = Direction.DR;
+		else if(!bU && !bD && bL && !bR) dir = Direction.L;
+		else if(!bU && !bD && !bL && bR) dir = Direction.R;
+		else if(!bU && !bD && !bL && !bR) dir = Direction.STOP;
+	}
+	
+	public void move() {
+		switch(dir) {
+		case U :
+			y -= SPEED;
+			break;
+		case UL :
+			x -= SPEED;
+			y -= SPEED;
+			break;
+		case UR :
+			x += SPEED;
+			y -= SPEED;
+			break;
+		case D :
+			y += SPEED;
+			break;
+		case DL :
+			x -= SPEED;
+			y += SPEED;
+			break;
+		case DR :
+			x += SPEED;
+			y += SPEED;
+			break;
+		case L :
+			x -= SPEED;
+			break;
+		case R :
+			x += SPEED;
+			break;
+		case STOP :
 			break;
 		}
 	}
